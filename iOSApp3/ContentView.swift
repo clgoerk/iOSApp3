9b5e342149507580ba06440b4db4dd214151f1c6
clgoerk <Christopherlee.Goerk@triosstudent.com>
2025-02-20 07:30:39 -0500

    Initial Commit

---
+++ iOSApp3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp3
+//
+//  Created by Chris Goerk on 2025-02-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp3/iOSApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp3App.swift
+//  iOSApp3
+//
+//  Created by Chris Goerk on 2025-02-20.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
